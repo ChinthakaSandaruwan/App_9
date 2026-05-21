6d3d2ff880b59c1eb7e652eee313de0cde742526
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-05-21 17:04:33 +0530

    feat: initialize project with package.json, reset script, and TypeScript configuration
    - Added package.json to manage project dependencies and scripts.
    - Created reset-project.js script to reset the project structure and move existing files.
    - Introduced tsconfig.json for TypeScript configuration with strict mode and path aliases.

---
+++ app/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+
+export default function Index() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.text}>Hello</Text>
+
+            <TouchableOpacity 
+                style={styles.btn}
+                onPress={() => {
+                    console.log("Log1");
+                    console.log("Log2");
+
+                   
+                    setTimeout(() => {
+                      console.log("Log3");
+                    }, 5000);
+
+                    
+                    console.log("Log4");
+                    console.log("Log5");
+                }}
+            >
+                <Text style={styles.btnTxt}>Button1</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.btn}>
+                <Text style={styles.btnTxt}>Button2</Text>
+            </TouchableOpacity>
+
+
+               <TouchableOpacity style={styles.btn}>
+                <Text style={styles.btnTxt}>Button3</Text>
+            </TouchableOpacity>
+
+
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 20,
+
+    },
+    btn: {
+        backgroundColor: "blue",
+        padding: 10,
+        margin: 10,
+        borderRadius: 5,
+    },
+    btnTxt: {
+        fontSize: 16,
+                   color: 'white',
+
+    },
+});
