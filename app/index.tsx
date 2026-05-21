ccc5de29fca48a6ae58ccc5b2c3f0fac4c46f8be
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-05-21 18:59:36 +0530

    refactor: clean up formatting and improve readability in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,63 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView } from "react-native";
 
 export default function Index() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>Hello</Text>
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
 
-            <TouchableOpacity 
-                style={styles.btn}
-                onPress={() => {
-                    console.log("Log1");
-                    console.log("Log2");
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => {
+          console.log("Log1");
+          console.log("Log2");
+          let data = "";
 
-                   
-                    setTimeout(() => {
-                      console.log("Log3");
-                    }, 5000);
+          setTimeout(() => {
+            data = "Hello World";
+            console.log("Log3");
+          }, 3000);
 
-                    
-                    console.log("Log4");
-                    console.log("Log5");
-                }}
-            >
-                <Text style={styles.btnTxt}>Button1</Text>
-            </TouchableOpacity>
+          console.log("Log4");
+          console.log(data);
+          console.log("Log5");
+        }}
+      >
+        <Text style={styles.btnTxt}>Button1</Text>
+      </TouchableOpacity>
 
-            <TouchableOpacity style={styles.btn}>
-                <Text style={styles.btnTxt}>Button2</Text>
-            </TouchableOpacity>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnTxt}>Button2</Text>
+      </TouchableOpacity>
 
-
-               <TouchableOpacity style={styles.btn}>
-                <Text style={styles.btnTxt}>Button3</Text>
-            </TouchableOpacity>
-
-
-        </SafeAreaView>
-    );
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnTxt}>Button3</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    text: {
-        fontSize: 20,
-
-    },
-    btn: {
-        backgroundColor: "blue",
-        padding: 10,
-        margin: 10,
-        borderRadius: 5,
-    },
-    btnTxt: {
-        fontSize: 16,
-                   color: 'white',
-
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+  btn: {
+    backgroundColor: "blue",
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    width: "75%",
+  },
+  btnTxt: {
+    fontSize: 16,
+    color: "white",
+  },
 });
